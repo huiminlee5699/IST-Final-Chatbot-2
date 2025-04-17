556746d522f007b5bb8154bd9c61608f2220ba59
Hui Min Lee <145514692+huiminlee5699@users.noreply.github.com>
2025-04-17 14:41:17 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,12 +57,11 @@ if prompt := st.chat_input("What would you like to know today?"):
         # If this is after the second assistant message (2nd, 4th, etc.), prepend the message
         prepend_message = ""
         if len(assistant_messages) == 1:  # Changed condition to display after 2nd response
-            prepend_message = (
+            prepend_message =(
                 "💡🧠🤓 <strong>Want to learn how I come up with responses?</strong>\n"
                 "    <a href=\"https://www.figma.com/proto/haXTVr4wZaeSC344BqDBpR/Text-Transparency-Card?page-id=0%3A1&node-id=1-33&p=f&viewport=144%2C207%2C0.47&t=Hp8ZCw5Fg7ahsiq1-8&scaling=min-zoom&content-scaling=fixed&hide-ui=1" target=\"_blank\" style=\"color: #007BFF; text-decoration: none;\">\n"
                 "        Read more here →\n"
-                "    </a>\n\n ---------------- \n" 
-            )
+                "    </a>\n\n ---------------- \n")
             
             full_response += prepend_message
             response_container.markdown(full_response, unsafe_allow_html=True)
