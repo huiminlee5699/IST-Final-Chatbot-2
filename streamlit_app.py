094549e9f710ae64b851245c7af20964b3e6f8db
Hui Min Lee <145514692+huiminlee5699@users.noreply.github.com>
2025-04-23 22:28:59 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,30 @@ st.set_page_config(
     page_title="💬 CHATBOT AI",
 )
 
+<script>
+(function (m, a, z, e) {
+  var s, t;
+  try {
+    t = m.sessionStorage.getItem('maze-us');
+  } catch (err) {}
+
+  if (!t) {
+    t = new Date().getTime();
+    try {
+      m.sessionStorage.setItem('maze-us', t);
+    } catch (err) {}
+  }
+
+  s = a.createElement('script');
+  s.src = z + '?apiKey=' + e;
+  s.async = true;
+  a.getElementsByTagName('head')[0].appendChild(s);
+  m.mazeUniversalSnippetApiKey = e;
+})(window, document, 'https://snippet.maze.co/maze-universal-loader.js', '16abf1fc-3397-439b-9561-75896a7f1306');
+</script>
+
+st.markdown(maze_script, unsafe_allow_html=True)
+
 st.markdown("""
 <style>
     /* Import fonts */
